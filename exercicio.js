c51b11dcb2697e6c35bab4f57f8c905640df57c0
Kaio Lima <112725257+kaiolima77@users.noreply.github.com>
2022-09-08 13:26:59 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,10 @@
+let nome, diabete, peso, altura, internacao
+
+    nome = prompt("Escreva seu nome: ")
+    diabete = prompt("Você tem diabete? ")
+    peso = prompt("Quanto você pesa? ")
+    altura = prompt("Qual é a sua altura? ")
+    internacao = prompt("Qual é a data que você foi internado/a? ")
+    console.log("O nome do paciente é: " + nome + " anos" + "\nO paciente pesa: " + peso + "\nA altura do paciente é: " + altura + "metros" + "\nA data de internação do paciente é: " + internacao + "\nO paciente é diabético? " + diabete)
+    
+        
