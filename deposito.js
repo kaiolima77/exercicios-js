79868f04ba0f6ce4d8d15f3b4a78584dd19fc472
kaiolima77 <kaiolimadecarvalho@gmail.com>
2022-09-21 11:04:38 -0300

    Exercicios08-09

---
+++ deposito.js
@@ -0,0 +1,7 @@
+let deposito, rendimento
+
+deposito = prompt("Informe o valor investido: ")
+
+rendimento = parseInt(deposito) * (0.007) + parseInt(deposito)
+
+alert(parseFloat (deposito)  + rendimento)
---
+++ numeros.js
@@ -0,0 +1,10 @@
+let numero
+
+numero = prompt("Informe um número: ");
+
+if (parseFloat (numero) > 80 || (numero) < 25 || (numero) == 40){
+    alert("Acertou!");
+}
+else{
+    alert("Errou!");
+}
